9243d00a85856df8c8073c6ff86abbafe34b4af2
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-11-16 18:14:32 +0100

    add context provider

--- p14-react/src/App.jsx
+++ p14-react/src/App.jsx
@@ -4,20 +4,23 @@ import FormUI from "./components/FormUI";
 import TableUI from "./components/TableUI";
 import Error from "./components/Error";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { EmployeeContextProvider } from "./context/EmployeeContext";
 
 function App() {
   return (
-    <Router>
-      <div className="m-4">
-        <Header />
-        <Title />
-        <Routes>
-          <Route path="/" element={<FormUI />} />
-          <Route path="/employees" element={<TableUI />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-    </Router>
+    <EmployeeContextProvider>
+      <Router>
+        <div className="m-4">
+          <Header />
+          <Title />
+          <Routes>
+            <Route path="/" element={<FormUI />} />
+            <Route path="/employees" element={<TableUI />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </div>
+      </Router>
+    </EmployeeContextProvider>
   );
 }
 
