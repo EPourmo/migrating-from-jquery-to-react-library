84d4a27227fd995c953161ea317b5978422934b8
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-11-16 15:20:48 +0100

    add required rules and work on retrived data

--- p14-react/src/components/FormUI.jsx
+++ p14-react/src/components/FormUI.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, InputNumber, message, Select } from "antd";
+import { Button, Form, InputNumber, Select } from "antd";
 import "antd/dist/antd.css";
 import { stateData, departmentData, forItemData } from "../formData";
 import Components from "../formData";
@@ -7,12 +7,38 @@ import Modal from "./Modal";
 import { useState } from "react";
 
 const FormUI = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [employees, setEmployees] = useState([]);
+  const [form] = Form.useForm();
+
   const getData = (e) => {
-    console.log(e);
+    const strDate = e.startDate._d;
+    const strDay = strDate.getDate();
+    const strMonth = strDate.getMonth();
+    const strYear = strDate.getFullYear();
+    const birthDate = e.birth._d;
+    const birthDay = birthDate.getDate();
+    const birthMonth = birthDate.getMonth();
+    const birthYear = birthDate.getFullYear();
+    const newEmployee = {
+      key: employees.length + 1,
+      firtName: e.firstName,
+      lastName: e.lastName,
+      startDate: `${strYear}/${strMonth + 1}/${strDay}`,
+      department: e.department,
+      birth: `${birthYear}/${birthMonth + 1}/${birthDay}`,
+      street: e.street,
+      city: e.city,
+      state: e.state,
+      zipCode: e.zip,
+    };
+
+    setEmployees((prevEmployee) => [...prevEmployee, newEmployee]);
+
+    form.resetFields();
   };
 
-  const [modalOpen, setModalOpen] = useState(false);
-
+  console.log(employees);
   return (
     <>
       <Form
@@ -25,9 +51,19 @@ const FormUI = () => {
         layout="horizontal"
         size="large"
         onFinish={getData}
+        form={form}
       >
         {forItemData.map((block) => Components(block))}
-        <Form.Item label="State" name="state">
+        <Form.Item
+          label="State"
+          name="state"
+          rules={[
+            {
+              required: true,
+              message: `Please input your State!`,
+            },
+          ]}
+        >
           <Select
             showSearch
             placeholder="Search state"
@@ -43,11 +79,29 @@ const FormUI = () => {
             options={stateData}
           />
         </Form.Item>
-        <Form.Item label="Zip Code" name="zip">
+        <Form.Item
+          label="Zip Code"
+          name="zip"
+          rules={[
+            {
+              required: true,
+              message: `Please input your Zip Code!`,
+            },
+          ]}
+        >
           <InputNumber />
         </Form.Item>
 
-        <Form.Item label="Department" name="Ddpartment">
+        <Form.Item
+          label="Department"
+          name="department"
+          rules={[
+            {
+              required: true,
+              message: `Please input your Department`,
+            },
+          ]}
+        >
           <Select
             showSearch
             placeholder="Search department"
@@ -67,16 +121,24 @@ const FormUI = () => {
           wrapperCol={{
             span: 7,
           }}
+          shouldUpdate
         >
-          <Button
-            htmlType="submit"
-            color="black"
-            onClick={() => {
-              setModalOpen(true);
-            }}
-          >
-            Save
-          </Button>
+          {() => (
+            <Button
+              htmlType="submit"
+              color="black"
+              disabled={
+                !form.isFieldsTouched(true) ||
+                form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length > 0
+              }
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              Save
+            </Button>
+          )}
         </Form.Item>
       </Form>
       {modalOpen && (
