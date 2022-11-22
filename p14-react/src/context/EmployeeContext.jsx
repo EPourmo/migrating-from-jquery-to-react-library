1aab3414ef95a8468fa8da48bf1864e79e6881d6
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-11-22 15:26:59 +0100

    comments code

--- p14-react/src/context/EmployeeContext.jsx
+++ p14-react/src/context/EmployeeContext.jsx
@@ -1,9 +1,22 @@
 import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+// create Context
 const EmployeeContext = createContext();
 
+/**
+ * return a new component which will wrap children components to pass context value
+ * @param {Object} props children components
+ * @returns
+ */
 function EmployeeContextProvider(props) {
+  // create usState to retrieve employees data
   const [employees, setEmployees] = useState([]);
 
+  /**
+   * funtion to add new employee data
+   * @param {Object} newEmployee new employee data
+   */
   function addEmployee(newEmployee) {
     setEmployees((prevEmployee) => [...prevEmployee, newEmployee]);
   }
@@ -16,3 +29,7 @@ function EmployeeContextProvider(props) {
 }
 
 export { EmployeeContextProvider, EmployeeContext };
+
+EmployeeContextProvider.propTypes = {
+  children: PropTypes.node,
+};
